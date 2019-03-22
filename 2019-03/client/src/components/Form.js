74bab6d115be1a9e3b5223ab1d6eb6d47ddea890
sbardascione <crmsal81@gmail.com>
2019-03-22 17:10:46 +0100

    sbrinzel logo

--- 2019-03/client/src/components/Form.js
+++ 2019-03/client/src/components/Form.js
@@ -4,6 +4,17 @@ import PropTypes from 'prop-types'
 import Input from './Input'
 import InputSubmit from './InputSubmit'
 
+import logo from './sbrinzel_logo.png'
+
+function Footer() {
+    return (
+        <div id="footer">
+            <span>powered by</span>
+        <img height="20" width="20" src={logo} alt="Logo" />
+        </div>
+    )
+}
+
 const Form = (props) => {
     const { onSubmit, onChange, onChangeDate, formData, formFields } = props
     return (
@@ -13,29 +24,30 @@ const Form = (props) => {
                 key={ inputName.toString() }
               inputLabel={ input.label }
               inputType={ input.type }
-              inputName={ inputName } 
+              inputName={ inputName }
               inputId={ input.id }
               isValid={input.isValid}
               errorMessage={input.errorMessage}
               isTouched={input.isTouched}
-              inputValue={ formData[ inputName ] } 
-              inputPlaceholder={ input.placeholder } 
+              inputValue={ formData[ inputName ] }
+              inputPlaceholder={ input.placeholder }
               onChange={ onChange }
               onChangeDate={ onChangeDate }
               />
             })
             }
             <InputSubmit />
+            <Footer/>
         </form>
     )
 }
 
 Form.propTypes = {
-  onSubmit: PropTypes.func.isRequired, 
-  onChange: PropTypes.func.isRequired, 
-  onChangeDate: PropTypes.func.isRequired, 
-  formData: PropTypes.objectOf(PropTypes.any).isRequired, /* to do: refine the shape */ 
-  formFields: PropTypes.objectOf(PropTypes.any).isRequired, /* to do: refine the shape */ 
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onChangeDate: PropTypes.func.isRequired,
+  formData: PropTypes.objectOf(PropTypes.any).isRequired, /* to do: refine the shape */
+  formFields: PropTypes.objectOf(PropTypes.any).isRequired, /* to do: refine the shape */
 };
 
 export default Form
